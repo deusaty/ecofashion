5e748b46f55dbdb5af8a3d31247c2cdfc1dcd0c3
deusaty <deusaty@gmail.com>
2026-05-16 20:49:42 -0600

    parallax1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Mainheader } from './components/mainheader/mainheader'; 
+import { Mainheader } from './components/mainheader/mainheader';
 import { NavbarComponent } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
-import { Infografia } from './components/infografia/infografia';  
-import { ComparatorComponent } from './components/comparator/comparator';           
+import { Infografia } from './components/infografia/infografia';
+import { ComparatorComponent } from './components/comparator/comparator';
+
+declare var M: any;
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,11 @@ import { ComparatorComponent } from './components/comparator/comparator';
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('GreenFashion');
+
+  ngAfterViewInit(): void {
+    const elems = document.querySelectorAll('.parallax');
+    M.Parallax.init(elems, {});
+  }
 }
